8c5baca2f761c1dc4cc16d1efa12cf25b1d1fdb5
PhamTuanDat <pdat100111@gmail.com>
2026-03-05 17:13:34 +0700

    init project

---
+++ src/main/java/vn/com/linkjob/LinkjobApplication.java
@@ -0,0 +1,13 @@
+package vn.com.linkjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkjobApplication.class, args);
+	}
+
+}
