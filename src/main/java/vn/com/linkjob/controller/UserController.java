f6bae01ac78e97e0076736fc2bd2b6503002777f
PhamTuanDat <pdat100111@gmail.com>
2026-03-06 19:59:20 +0700

    feat: api get all user

--- src/main/java/vn/com/linkjob/controller/UserController.java
+++ src/main/java/vn/com/linkjob/controller/UserController.java
@@ -6,13 +6,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import vn.com.linkjob.dto.user.CreateUserRequestDTO;
 import vn.com.linkjob.dto.user.UserResponseDTO;
 import vn.com.linkjob.service.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -26,5 +25,12 @@ public class UserController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(userService.createUser(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getAllUser() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userService.getAllUser());
     }
 }
--- src/main/java/vn/com/linkjob/service/UserService.java
+++ src/main/java/vn/com/linkjob/service/UserService.java
@@ -7,8 +7,13 @@ import org.springframework.stereotype.Service;
 import vn.com.linkjob.domain.User;
 import vn.com.linkjob.dto.user.CreateUserRequestDTO;
 import vn.com.linkjob.dto.user.UserResponseDTO;
+import vn.com.linkjob.exception.AppException;
+import vn.com.linkjob.exception.ErrorCode;
 import vn.com.linkjob.mapper.UserMapper;
 import vn.com.linkjob.repository.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collector;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +29,11 @@ public class UserService {
         User newUser = userMapper.toUser(request);
 
         return userMapper.toUserResponseDTO(userRepository.save(newUser));
+    }
+
+    public List<UserResponseDTO> getAllUser() {
+        return userRepository.findAll().stream()
+                .map(userMapper::toUserResponseDTO)
+                .toList();
     }
 }
