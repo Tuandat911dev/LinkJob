b4dd7f2d3a3e970d391b96bc6d7e03e3f1917095
PhamTuanDat <pdat100111@gmail.com>
2026-03-13 16:58:05 +0700

    fix: add version for AuthController

--- src/main/java/vn/com/linkjob/controller/AuthController.java
+++ src/main/java/vn/com/linkjob/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vn.com.linkjob.dto.auth.LoginDTO;
 import vn.com.linkjob.dto.auth.LoginResponseDTO;
@@ -18,6 +19,7 @@ import vn.com.linkjob.util.SecurityUtil;
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RequestMapping("/api/v1")
 public class AuthController {
     AuthenticationManagerBuilder authenticationManagerBuilder;
     SecurityUtil securityUtil;
